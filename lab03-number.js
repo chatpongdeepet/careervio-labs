9baf5204543180d560f8ada50ff92e4d3cfe84c1
theDcreative <tangdesign.sci@gmail.com>
2022-05-21 21:00:57 +0700

    add Lab 3 : number exponential

---
+++ lab03-number.js
@@ -0,0 +1,28 @@
+// number to e
+let num1 = 1000000;
+num1 = num1.toExponential();
+let num2 = 10000000;
+num2 = num2.toExponential();
+let num3 = 43000000000;
+num3 = num3.toExponential();
+let num4 = 0.000012;
+num4 = num4.toExponential();
+let num5 = 0.0000001234;
+num5 = num5.toExponential();
+// e to number
+let num6 = 1e10;
+let num7 = 2e5;
+let num8 = 122e8;
+let num9 = 1e-8;
+let num10 = 12e-5;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(num8);
+console.log(num9);
+console.log(num10);
