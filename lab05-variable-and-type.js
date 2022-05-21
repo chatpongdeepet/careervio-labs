630bd7b69a3f78e6198dd333be9d5a0d12a19c57
theDcreative <tangdesign.sci@gmail.com>
2022-05-21 22:11:05 +0700

    rename lab04 file to lab05

---
+++ lab05-variable-and-type.js
@@ -0,0 +1,13 @@
+var var01 = false;
+var var02 = "ABD";
+var var03 = 113113;
+var var04 = null;
+var var05 = "";
+// var var06 = 13qed; ตัวเลขกับ string -->> error
+
+console.log(typeof var01);
+console.log(typeof var02);
+console.log(typeof var03);
+console.log(typeof var04);
+console.log(typeof var05);
+console.log(typeof var06);
