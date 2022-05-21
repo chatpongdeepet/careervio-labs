16770e162c1f7785578da5b4d0ea0a72c21fc5d4
theDcreative <tangdesign.sci@gmail.com>
2022-05-21 21:30:51 +0700

    add Lab3.2

---
+++ lab03-2-number.js
@@ -0,0 +1,29 @@
+// to Octal and Hexadecimal
+let num1 = 100;
+let num2 = 111;
+let num3 = 55;
+let num4 = 21;
+let num5 = 99;
+let num6 = 100;
+let num7 = 111;
+let num8 = 55;
+let num9 = 21;
+let num10 = 99;
+let num11 = 12.423;
+let num12 = 31313.135;
+let num13 = 42.809;
+
+// to Octal
+console.log(num1.toString(8));
+console.log(num2.toString(8));
+console.log(num3.toString(8));
+console.log(num4.toString(8));
+console.log(num5.toString(8));
+console.log(num6.toString(16));
+console.log(num7.toString(16));
+console.log(num8.toString(16));
+console.log(num9.toString(16));
+console.log(num10.toString(16));
+console.log(Math.round(num11));
+console.log(Math.round(num12));
+console.log(Math.round(num13));
