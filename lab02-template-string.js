1bb6a72d43ceec41510b86e88164b9a6bcf52e2b
theDcreative <tangdesign.sci@gmail.com>
2022-05-21 20:48:33 +0700

    add Lab 2

---
+++ lab02-template-string.js
@@ -0,0 +1,9 @@
+let str1 = 'microsoft excel';
+let str2 = 'GOOGLE AND APPLE';
+let str3 = 'hello world';
+
+str3Change = str3.replace("h","H").replace("w","W");
+
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+console.log(str3Change);
