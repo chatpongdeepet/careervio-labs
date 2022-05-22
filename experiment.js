1b95abd06646f42dd3e7987633cc67366d12912a
theDcreative <tangdesign.sci@gmail.com>
2022-05-22 12:36:22 +0700

    lab08 edit and add expiriment file

--- experiment.js
+++ experiment.js
@@ -8,10 +8,16 @@
 
 // console.log(user.name);
 
-let user = {
-    name: "John",
-    age: 30
-};
+// let user = {
+//     name: "John",
+//     age: 30
+// };
 
-let clone = Object.assign({}, user);
-console.log(clone)
+// let clone = Object.assign({}, user);
+// console.log(clone)
+
+// Loop with array
+let arr = ["Apple", "Orange", "Pear"];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
