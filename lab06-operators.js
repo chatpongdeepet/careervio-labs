307efec680897a9c3a36f748b65cf60462badacd
theDcreative <tangdesign.sci@gmail.com>
2022-05-22 11:11:00 +0700

    add lab6 operators

---
+++ lab06-operators.js
@@ -0,0 +1,25 @@
+// sting to num
+// จงแปลงค่าของ String "230" ให้เป็น number แล้ว print ออกมา
+let str1 = "230";
+num1 = Number(str1);
+
+console.log(typeof num1);
+
+// number to string
+// จงแปลงค่าจอง Number 230 ให้เป็น String แล้ว print ออกมา 
+let num2 = 230;
+str2 = String(num2);
+
+console.log(typeof str2);
+
+
+// exponet
+// จงเขียน code เพื่อหา 3 ยกกำลัง 4 แล้ว print ออกมาก
+let num3 = 3 ** 4;
+console.log(num3);
+
+// compound assignment operators
+// จงเขียน code เพื่อให้ ค่า x เท่ากับ 6 แล้วนำ x มาคูณ 20 โดยใช้ "การดำเนินการแบบย่อ"
+let x = 6;
+x *= 20;
+console.log(x);
